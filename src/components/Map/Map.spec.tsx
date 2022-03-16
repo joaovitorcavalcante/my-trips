f93b25e9379dcbddea9b86db5ea0d31ddf926e38
JOAO VITOR CAVALCANTE BARBOSA <117310@pmenos.com.br>
2022-03-16 11:21:30 -0300

    test component map

---
+++ src/components/Map/Map.spec.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react';
+import Map from '.';
+
+describe('<Map />', () => {
+  it('should render without any marker', () => {
+    render(<Map />);
+
+    expect(
+      screen.getByRole('link', {
+        name: /a js library for interactive maps/i
+      })
+    ).toBeInTheDocument();
+  });
+
+  it('should render with the marker in correct place', () => {
+    const places = [
+      {
+        id: '1',
+        name: 'Petrópolis',
+        slig: 'petrópolis',
+        location: {
+          latitude: 0,
+          longitude: 0
+        }
+      }
+    ];
+
+    render(<Map places={places} />);
+
+    expect(screen.getByTitle(/petrópolis/i)).toBeInTheDocument();
+  });
+});
--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,8 +1,20 @@
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { MapContainer, Marker, TileLayer } from 'react-leaflet';
 
-const Map = () => {
-  const position = [51.505, -0.09];
+type Place = {
+  id: string;
+  name: string;
+  slug: string;
+  location: {
+    latitude: number;
+    longitude: number;
+  };
+};
 
+type MapProps = {
+  places: Place[];
+};
+
+const Map = ({ places }: MapProps) => {
   return (
     <MapContainer
       center={[0, 0]}
@@ -13,11 +25,13 @@ const Map = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[0, 0]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      {places?.map(({ id, name, location: l }) => (
+        <Marker
+          key={`place-${id}`}
+          title={name}
+          position={[l.latitude, l.longitude]}
+        />
+      ))}
     </MapContainer>
   );
 };
